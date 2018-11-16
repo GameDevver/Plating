3f84b0debad98491fbe831286b510c71b5f9a6b0
GamDevv <borsik.gr@gmail.com>
2018-11-16 22:35:23 +0300

    +Plating

---
+++ Plating/Assets/Scripts/Move.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    public float speed = 0.25f; // скорость движения персонажа
+
+    void Update()
+    {
+        float deltaX = Input.GetAxis("Horizontal") * speed; // зайди в Unity: Edit->Project Settings->Input ; в Inspector появится вкладка, где можно понять откуда я брал обозначения "Horizontal", "Vertical", "Mouse X","Mouse Y".
+        float deltaZ = Input.GetAxis("Vertical") * speed;
+        transform.Translate(deltaX, 0, deltaZ); // Translate - метод для перемещения персонажа. В данном случае в плоскости X0Z.
+    }
+}
+
